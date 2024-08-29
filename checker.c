f1ee7aeac608a723db89f37f8c2e93b68232c61f
Sharaj5 <ssharaj2@gmail.com>
2024-08-29 15:13:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,5 +7,5 @@ return ((temperature >0 || temperature < 45) || (soc > 20 || soc <80) || (charge
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(!=batteryIsOk(50, 85, 0));
 }
