e9e365a28901627b52eaec7ea51a2489bcb61863
Sharaj5 <ssharaj2@gmail.com>
2024-09-04 12:17:15 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 #include <assert.h>
 
-int is_Within_Range(float value, float minimum, float maximum) {
-  return (value >= minimum && value <= maximum);
+int isTemperatureOk(float temperature) {
+    return temperature < 0 || temperature > 45;
 }
 
-int check_scope(float value, float minimum, float maximum, const char* errorMessage) {
-  if (!is_Within_Range(value, minimum, maximum)) {
-    printf("%s\n", errorMessage);
-    return 0;
-  }
-  return 1;
+int isSocOk(float soc) {
+    return soc < 20 || soc > 80;
 }
 
+int isChargeRateOk(float chargeRate) {
+    return chargeRate > 0.8;
+}
+
+void disp_temp(int temp_check){
+    
+    if(!temp_check){
+        printf("Temperature out of range!\n");
+    }
+}
+void disp_soc(int soc_check){
+    
+    if(!soc_check){
+        printf("State of Charge out of range!\n");
+    }
+}
+void disp_cr(int cr_check){
+    
+    if(!cr_check){
+        printf("Charge Rate out of range!\n");
+    }
+}
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  int Tremp_Ok = check_scope(temperature, 0, 45, "The temperature is out of range...");
-  int Soc_Ok = check_scope(soc, 20, 80, "The state of Charge is out of range...");
-  int ChargeRate_Ok = check_scope(chargeRate, 0, 0.8, "The charge Rate is out of range...");
-
-  return Tremp_Ok && Soc_Ok && ChargeRate_Ok;
+    int temp_ok = !isTemperatureOk(temperature);
+    int soc_ok = !isSocOk(soc);
+    int charge_ok = !isChargeRateOk(chargeRate);
+    
+    disp_temp(temp_ok);
+    disp_soc(soc_ok);
+    disp_cr(charge_ok);
+    
+    return 1;
 }
-
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7));//no print
+    assert(batteryIsOk(55, 50, 0.2));//temp out of range
+    assert(batteryIsOk(-1, 75, 0.0));//temp out of range
+    assert(batteryIsOk(20, 90, 0.2));//Soc Out of range
+    assert(batteryIsOk(20, 15, 0.2));//Soc Out of range
+    assert(batteryIsOk(25, 25, 0.8));//charge out of range
 }
