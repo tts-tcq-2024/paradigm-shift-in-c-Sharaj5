7c4fccf157365df6d8113259ffcae35dc696e9ff
Sharaj5 <ssharaj2@gmail.com>
2024-09-04 17:02:30 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,60 +1,24 @@
-using System;
-using System.Diagnostics;
-namespace paradigm_shift_csharp
-{
-class Checker
-{
-     static bool isTemperatureWithInTheRange(float temperature)
-     {
-           if (temperature < 0 || temperature > 45)
-         {
-             Console.WriteLine("Temperature is out of range!");
-             return false;
-         }
-     return true;
-     }
-
-     static bool isSocWithInTheRange(float soc)
-     {
-        if (soc < 20 || soc > 80)
-         {
-             Console.WriteLine("State of Charge is out of range!");
-             return false;
-         }
-     return true;
-     }
-
-     static bool isChargeRateWithInTheRange(float chargeRate)
-     {
-          if (chargeRate > 0.8)
-         {
-             Console.WriteLine("Charge Rate is out of range!");
-             return false;
-         }
-     return true;
-     }
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-     return isTemperatureWithInTheRange(temperature) && isSocWithInTheRange(soc) && isChargeRateWithInTheRange(chargeRate);     
+#include <stdio.h>
+#include <assert.h>
+ 
+int isParameterWithinLimits(float value, float min, float max, const char* message) {
+    if (value < min || value > max) 
+    {
+        printf("%s\n", message);
+        return 1;
     }
-
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
-    static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
-        Console.WriteLine("All ok");
-        return 0;
-    }
-    
+    return 0;
 }
+ 
+int batteryStateOk(float temperature, float soc, float chargeRate) {
+    int isTempWithinLimits = isParameterWithinLimits(temperature, 15, 45, "Temperature within Limits!");
+    int isSocWithinLimits = isParameterWithinLimits(soc, 20, 80, "State of Charge within Limits!");
+    int isChargeRateWithinLimits = isParameterWithinLimits(chargeRate, 0, 0.8, "Charge Rate within Limits!");
+ 
+    return !(isTempWithinLimits || isSocWithinLimits || isChargeRateWithinLimits);
+}
+ 
+int main() {
+    assert(batteryStateOk(25, 70, 0.7));
+    assert(!batteryStateOk(50, 85, 0));
 }
